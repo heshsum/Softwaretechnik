70a695229734b37199430504cbc65f39a70b207c
heshsum <mail@heshsum.de>
2022-06-12 13:52:38 +0200

    Added ESA 8

---
+++ ESA 8/src/main/App.java
@@ -0,0 +1,18 @@
+package org.pareto;
+
+import static org.pareto.LogHelper.log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    /**
+     * main method of the app.
+     * @author heshsum
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        log("Hello World!");
+    }
+}
---
+++ ESA 8/src/test/AppTest.java
@@ -0,0 +1,19 @@
+package org.pareto;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue("5 is greater then 4", 5 > 4);
+    }
+}
